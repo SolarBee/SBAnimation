69c0507ed01df47a9080eef59ebacc3e43ba95a9
孙扬 <624162319@qq.com>
2017-01-12 16:34:37 +0800

    add some comments

--- SBAnimation/SBAnimation/SpeedMeterView/SpeedMeterTestViewController.m
+++ SBAnimation/SBAnimation/SpeedMeterView/SpeedMeterTestViewController.m
@@ -2,8 +2,11 @@
 //  SpeedMeterTestViewController.m
 //  SBAnimation
 //
-//  Created by 孙扬 on 2017/1/12.
+//  Created by Programmer Sunny on 2017/1/12.
 //  Copyright © 2017年 Programmer Sunny. All rights reserved.
+//
+//  This source code is licensed under the MIT-style license found in the
+//  LICENSE file in the root directory of this source tree.
 //
 
 #import "SpeedMeterTestViewController.h"
